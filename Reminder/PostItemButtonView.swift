6fc6fc2128abb0e258c913971f765631d8634299
YeoSungeun <dutjddms@naver.com>
2024-07-03 08:29:10 +0900

    [Feat] realm에 title 넣기

--- Reminder/PostItemButtonView.swift
+++ Reminder/PostItemButtonView.swift
@@ -9,4 +9,22 @@ import UIKit
 
 class PostItemButtonView: UIView {
     
+    init(title: String) {
+        super.init(frame: .zero)
+        configureHierarchy()
+        configureLayout()
+        configureView()
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    func configureHierarchy() {
+        
+    }
+    func configureLayout() {
+        
+    }
+    func configureView() {
+        
+    }
 }
--- Reminder/Screen/Post/PostViewController.swift
+++ Reminder/Screen/Post/PostViewController.swift
@@ -10,16 +10,30 @@ import RealmSwift
 
 final class PostViewController: BaseViewController {
     
+    let titleTextField = {
+        let view = UITextField()
+        view.placeholder = "제목"
+        return view
+    }()
+    let memoTextView = {
+        let view = UITextView()
+        view.text = "메모"
+        return view
+    }()
+    let contentView = UIView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
     override func configureHierarchy() {
-        
+        view.addSubview(titleTextField)
     }
     override func configureLayout() {
-         
+        titleTextField.snp.makeConstraints { make in
+            make.top.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+            make.height.equalTo(32)
+        }
     }
     override func configureView() {
         super.configureView()
@@ -29,6 +43,8 @@ final class PostViewController: BaseViewController {
         navigationItem.leftBarButtonItem = cancelButton
         let addButton = UIBarButtonItem(title: "추가", style: .plain, target: self, action: #selector(addButtonClicked))
         navigationItem.rightBarButtonItem = addButton
+        
+        titleTextField.backgroundColor = .systemGray2
     }
     
     @objc func cancelButtonClicked() {
@@ -40,12 +56,19 @@ final class PostViewController: BaseViewController {
         print(#function)
         let realm = try! Realm()
         
-        let data = Todo(title: "이것저것", memo: "요렇게저렇게", dudate: "2022.08,12")
+        guard let title = titleTextField.text, !title.isEmpty else {
+            showAlert(title: "제목을 입력해 주세요", message: "", ok: "확인") {
+                print("alert")
+            }
+            return
+        }
+        
+        let data = Todo(title: title, memo: "요렇게저렇게", dudate: "2022.08,12")
         try! realm.write {
             realm.add(data)
             print("Realm Create Succeed")
         }
-        navigationController?.popViewController(animated: true)
+        dismiss(animated: true)
     }
 
 }
