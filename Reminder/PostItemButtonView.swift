df40289a5b638bc74ab309e5d109f9831e38007e
YeoSungeun <dutjddms@naver.com>
2024-07-04 00:38:49 +0900

    [Chore] PostItmeButtonView customview 생성

--- Reminder/PostItemButtonView.swift
+++ Reminder/PostItemButtonView.swift
@@ -6,25 +6,60 @@
 //
 
 import UIKit
+import SnapKit
 
 class PostItemButtonView: UIView {
+    let titleLabel = UILabel()
+    let detailLabel = UILabel()
+    let chevronImageVeiw = UIImageView()
+    let moreButton = UIButton()
     
     init(title: String) {
         super.init(frame: .zero)
         configureHierarchy()
         configureLayout()
-        configureView()
+        configureView(title: title)
     }
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     func configureHierarchy() {
-        
+        self.addSubview(titleLabel)
+        self.addSubview(detailLabel)
+        self.addSubview(chevronImageVeiw)
+        self.addSubview(moreButton)
+    
     }
     func configureLayout() {
+        titleLabel.snp.makeConstraints { make in
+            make.centerY.equalTo(self)
+            make.leading.equalTo(self.safeAreaLayoutGuide).offset(16)
+        }
+        chevronImageVeiw.snp.makeConstraints { make in
+            make.trailing.equalTo(self.safeAreaLayoutGuide).inset(12)
+            make.verticalEdges.equalTo(self.safeAreaLayoutGuide).inset(12)
+            make.width.equalTo(chevronImageVeiw.snp.height).multipliedBy(0.5)
+        }
+        detailLabel.snp.makeConstraints { make in
+            make.centerY.equalTo(self.snp.centerY)
+            make.trailing.equalTo(chevronImageVeiw.snp.leading).offset(-8)
+            make.leading.equalTo(titleLabel.snp.trailing).offset(20)
+        }
+        moreButton.snp.makeConstraints { make in
+            make.edges.equalTo(self)
+        }
         
     }
-    func configureView() {
-        
+    func configureView(title: String) {
+        self.backgroundColor = .systemBackground
+        self.clipsToBounds = true
+        self.layer.cornerRadius = 10
+        titleLabel.text = title
+        titleLabel.font = .systemFont(ofSize: 15)
+        detailLabel.textAlignment = .right
+        detailLabel.textColor = .systemGray2
+        detailLabel.font = .systemFont(ofSize: 13)
+        chevronImageVeiw.image = UIImage(systemName: "chevron.forward")
+        chevronImageVeiw.tintColor = .systemGray2
     }
 }
