8dc17d7ae506fa1747bc3f2aa9f5aeeb8f12d236
YeoSungeun <dutjddms@naver.com>
2024-07-12 21:41:02 +0900

    [Add] TagViewModel 로 로직분리

--- Reminder/ViewModel/TagViewModel.swift
+++ Reminder/ViewModel/TagViewModel.swift
@@ -7,7 +7,25 @@
 
 import Foundation
 
-class TagViewModel {
+final class TagViewModel {
     
+    var inputTag: Observable<String?> = Observable(nil)
+    var outputTag: Observable<String?> = Observable(nil)
+    var inputBackButtonClicked: Observable<Void?> = Observable(nil)
+    var inputClosure: Observable<((String) -> Void)?> = Observable(nil)
     
+    init() {
+        inputTag.bind { _ in
+            self.getTag()
+        }
+        inputBackButtonClicked.bind { _ in
+            self.inputClosure.value?(self.outputTag.value ?? "")
+        }
+    }
+    func getTag() {
+        guard let tag = inputTag.value, !tag.isEmpty else {
+            return
+        }
+        outputTag.value = tag
+    }
 }
